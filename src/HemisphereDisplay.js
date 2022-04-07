f96f80d54515670b5e095a24c001aaa41d267fd2
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-04-07 13:41:33 +0200

    Add images based on hemisphere

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,7 +1,13 @@
-const HemisphereDisplay = (props) => {
+import northern from './images/northern-hemisphere.webp'
+import southern from './images/southern-hemisphere.jpeg'
+
+const HemisphereDisplay = ({ latitude }) => {
+    const hemisphere = latitude > 0 ? 'Northern Hemisphere' : 'Southern Hemisphere'
+    const image = latitude > 0 ? northern : southern
     return (
         <>
-            {props.latitude}
+            <img src={image} alt={hemisphere} />
+            <p>You are in {hemisphere}</p>
         </>
     )
 }
