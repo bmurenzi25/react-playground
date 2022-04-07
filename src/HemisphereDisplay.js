6cb4ce3dd748d770b189ca7994173627ffe319d9
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-04-07 14:06:06 +0200

    add semantic ui styles

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -17,8 +17,14 @@ const HemisphereDisplay = ({ latitude }) => {
     const {text, picture} = hemisphereConfig[hemisphere]
     return (
         <div className={hemisphere}>
-            <img src={picture} alt={hemisphere} />
-            <p>{text}</p>
+            <div className='ui raised text container segment'>
+                <div className='image'>
+                    <img src={picture} alt={hemisphere} />
+                </div>
+                <div className='ui teal bottom attached label'>
+                    <h1>{text}</h1>
+                </div>
+            </div>
         </div>
     )
 }
