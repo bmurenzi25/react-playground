8c6bcbcf03f95e1fe56a7bed37c7bb35636097cb
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-04-16 14:04:28 +0200

    Fix, Loading spinner image styles

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -3,7 +3,7 @@ import spinner from '../../img/spinner.gif'
 
 const Spinner = () => {
   return (
-    <img src={spinner} alt='' />
+    <img src={spinner} alt='Loading' style={{ width: '200px', margin: 'auto', display: 'block' }} />
   )
 }
 
