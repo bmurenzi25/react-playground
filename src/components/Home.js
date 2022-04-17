027f432f77834216adcabbc522601cb09d8ad851
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-04-17 11:13:06 +0200

    Add, Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div
+      className='ui raised very padded text container segment'
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className='ui header'>Home</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+        reiciendis temporibus dolorum odit rem consequatur consequuntur quaerat
+        facere deleniti, natus, dolore eius iusto adipisci, modi repudiandae
+        maxime illo ipsa. Labore?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
