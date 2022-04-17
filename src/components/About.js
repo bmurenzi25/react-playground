3eebe86a1279a188837aabf50dfc820723cbb964
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-04-17 11:18:48 +0200

    Add About, Contact and Navbar components

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div
+      className='ui raised very padded text container segment'
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className='ui header'>About</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+        reiciendis temporibus dolorum odit rem consequatur consequuntur quaerat
+        facere deleniti, natus, dolore eius iusto adipisci, modi repudiandae
+        maxime illo ipsa. Labore?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div
+      className='ui raised very padded text container segment'
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className='ui header'>Contact</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+        reiciendis temporibus dolorum odit rem consequatur consequuntur quaerat
+        facere deleniti, natus, dolore eius iusto adipisci, modi repudiandae
+        maxime illo ipsa. Labore?
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
